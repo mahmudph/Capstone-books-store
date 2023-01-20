692ee2e68ac7f479705006210f71370547dccea5
mahmud <mahmud120398@gmail.com>
2023-01-20 08:54:06 +0700

    refactor: update logic handling detail book

--- book/src/main/java/id/myone/book/presentation/detailBook/DetailBookViewModel.kt
+++ book/src/main/java/id/myone/book/presentation/detailBook/DetailBookViewModel.kt
@@ -5,10 +5,30 @@
 
 package id.myone.book.presentation.detailBook
 
-import androidx.lifecycle.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.liveData
+import id.myone.book.domain.usecase.GetBookFavoriteStatusUseCase
 import id.myone.book.domain.usecase.GetDetailBookUseCase
+import id.myone.book.domain.usecase.SetFavoriteBookUseCase
+import id.myone.core.domain.entity.BookDetail
 
-class DetailBookViewModel(private val getDetailBookUseCase: GetDetailBookUseCase) : ViewModel() {
+class DetailBookViewModel(
+    private val getDetailBookUseCase: GetDetailBookUseCase,
+    private val getFavoriteBookStatusUseCase: GetBookFavoriteStatusUseCase,
+    private val setFavoriteBookUseCase: SetFavoriteBookUseCase,
+) : ViewModel() {
 
     fun loadBookDetail(bookId: String) = getDetailBookUseCase(bookId).asLiveData()
+
+    fun getBookFavoriteStatus(bookId: String) = liveData {
+        val result = getFavoriteBookStatusUseCase(bookId)
+        emit(result)
+    }
+
+    fun setFavoriteBook(bookId: String, book: BookDetail, value: Boolean) = liveData {
+        val result = setFavoriteBookUseCase(bookId, book, value)
+        emit(result)
+    }
+
 }
